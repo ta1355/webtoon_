4b2406270d04560bdf4738a6ed10675fade08616
yoo <eorbs255@naver.com>
2025-07-24 19:33:51 +0900

    api 연동 테스트

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 2,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.green,
+        centerTitle: true,
+        title: Text(
+          "Today's Webtoons",
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.w400),
+        ),
+      ),
+    );
+  }
+}
